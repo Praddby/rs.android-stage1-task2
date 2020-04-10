fdc6181d688c6c07b9d856d4135069a5b19317a2
Yury Bialou <Pradd.by@gmail.com>
2020-04-10 22:15:02 +0300

    feat: add fun subtask 3

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -2,8 +2,27 @@ package subtask3
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val mutableList = a.toUpperCase().chunked(1) as MutableList
+        val immutableList = b.chunked(1)
+        if ( !mutableList.containsAll(immutableList) ) return "NO"
+
+        mutableList.retainAll(immutableList)
+        val arrayList = mutableListOf<String>()
+        var i = 0
+        var j = 0
+        do {
+            if (immutableList[j] == mutableList[i]) {
+                arrayList.add(immutableList[j])
+                i++
+                j++
+            } else
+                i++
+        } while(immutableList.size > j && mutableList.size > i)
+
+        return if (immutableList == arrayList)
+            "YES"
+        else
+            "NO"
     }
 }
