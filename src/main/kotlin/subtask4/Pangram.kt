2d475f20cc2999be08a5a49e0a0fe7f34d2c3aae
Yury Bialou <Pradd.by@gmail.com>
2020-04-11 13:37:59 +0300

    feat: add fun subtask 4

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -2,8 +2,58 @@ package subtask4
 
 class Pangram {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        return if (inputString.toLowerCase().chunked(1).containsAll(alphabet))
+            getPangram(inputString)
+        else
+            getNotPangram(inputString)
+    }
+
+    private val alphabet = listOf("a", "b", "c", "d", "e", "f", "g", "h",
+        "i", "j", "k", "l", "m", "n", "o", "p", "q",
+        "r", "s", "t", "u", "v", "w", "x", "y", "z")
+
+    private fun getPangram(str: String): String {
+        val list = str.split("\n"," ").filter{ c -> c.isNotEmpty() }.map { s ->
+            "${s.count{ c -> c == 'e' || c == 'a' || c== 'i' || c== 'o' || c == 'u' || c == 'y'}}$s "
+        }
+        val strPlusNumber = buildString {
+            for (s in list.sortedBy { it[0] })
+                append(s)
+        }
+        val newStr = strPlusNumber.map { c ->
+            if (listOf('a', 'e', 'i', 'o', 'u', 'y').contains(c))
+                c.toUpperCase()
+            else
+                c
+        }
+        return buildString {
+            for (s in newStr)
+                append(s)
+        }.trim()
+    }
+
+    private fun getNotPangram(str: String): String {
+        val list = str.split("\n", " ").filter{ c -> c.isNotEmpty() }.map { s ->
+            "${s.toLowerCase().count{ c ->
+                c == 'b' || c == 'c' || c == 'd' || c == 'f' || c == 'g' || c == 'h' ||
+                c == 'j' || c == 'k' || c == 'l' || c == 'm' || c == 'n' || c == 'p' || c == 'q' || 
+                c == 'r' || c == 's' || c == 't' || c == 'v' || c == 'w' || c == 'x' || c == 'z'
+            }}$s "
+        }
+        val strPlusNumber = buildString {
+            for (s in list.sortedBy { it[0] })
+                append(s)
+        }
+        val newStr = strPlusNumber.map { c ->
+            if (!listOf('a', 'e', 'i', 'o', 'u', 'y').contains(c))
+                c.toUpperCase()
+            else
+                c
+        }
+        return buildString {
+            for (s in newStr)
+                append(s)
+        }.trim()
     }
 }
