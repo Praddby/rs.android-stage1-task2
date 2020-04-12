d550cdbb81f3d79528627d74a91c1ff48f4665cb
Yury Bialou <Pradd.by@gmail.com>
2020-04-12 16:17:36 +0300

    feat: add fun subtask 5

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,53 @@
 package subtask5
 
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        return when (blockB) {
+            String::class -> getSumString(blockA)
+            Int::class -> getSumInt(blockA)
+            LocalDate::class -> getLatestDate(blockA)
+            else -> throw NotImplementedError("Not implemented")
+        }
+    }
+
+    private fun getLatestDate(blockA: Array<*>): Any {
+        val now = LocalDate.now()
+        var resultDate: Period = Period.ZERO
+        for (n in blockA) {
+            if (n is LocalDate) {
+                val period = Period.between(n, now)
+                if (resultDate.isZero) {
+                    resultDate = period
+                    continue
+                }
+                if (period.minus(resultDate).isNegative)
+                    resultDate = period
+            }
+        }
+        return now.minus(resultDate).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+    }
+
+    private fun getSumInt(blockA: Array<*>): Any{
+        var result = 0
+        for (n in blockA) {
+            if (n is Int)
+                result += n
+        }
+        return result
+    }
+
+    private fun getSumString(blockA: Array<*>): Any{
+        var result = ""
+        for (n in blockA) {
+            if (n is String)
+                result += n
+        }
+        return result
     }
 }
